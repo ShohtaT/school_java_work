4f09df14a7c386e492a2a0dcfcec935dcc71154d
Takahashi Shota <shohh6119@gmial.com>
2024-10-04 14:22:44 +0900

    Fix 1-1

--- src/lesson1_10_4/Practice1.java
+++ src/lesson1_10_4/Practice1.java
@@ -8,3 +8,15 @@ public class Practice1 {
     omikuji.tellFortune(r);
   }
 }
+
+class Omikuji {
+  public void tellFortune(double val) {
+    if (val > 0.7) {
+      System.out.println("Good!!");
+    } else if (val > 0.2 && val <= 0.7) {
+      System.out.println("So so.");
+    } else {
+      System.out.println("Bad...");
+    }
+  }
+}
