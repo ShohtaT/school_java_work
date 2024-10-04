0e11959bcc9a35f70ee78fef10f89a136d91f077
Takahashi Shota <shohh6119@gmial.com>
2024-10-04 13:53:36 +0900

    Fix

---
+++ src/lesson1_10_4/Practice1.java
@@ -0,0 +1,12 @@
+package lesson1_10_4;
+
+public class Practice1 {
+  public static void main(String[] args) {
+    System.out.printf("Hello and welcome!\n");
+
+    for (int i = 1; i <= 5; i++) {
+      System.out.println("i = " + i);
+      System.out.println("i * i = " + i * i);
+    }
+  }
+}
