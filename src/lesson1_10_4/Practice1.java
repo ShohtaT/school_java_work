3cc0367904e0f2535e83531f6059d0c220ada8f8
Takahashi Shota <shohh6119@gmial.com>
2024-10-04 14:11:28 +0900

    Fix 1-1

--- src/lesson1_10_4/Practice1.java
+++ src/lesson1_10_4/Practice1.java
@@ -2,11 +2,9 @@ package lesson1_10_4;
 
 public class Practice1 {
   public static void main(String[] args) {
-    System.out.printf("Hello and welcome!\n");
+    double r = Math.random();
 
-    for (int i = 1; i <= 5; i++) {
-      System.out.println("i = " + i);
-      System.out.println("i * i = " + i * i);
-    }
+    Omikuji omikuji = new Omikuji();
+    omikuji.tellFortune(r);
   }
 }
