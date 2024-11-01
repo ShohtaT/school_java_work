45ff099049d6fd766cdef8376455e37c73fe97e8
Takahashi Shota <shohh6119@gmial.com>
2024-11-01 14:20:34 +0900

    Done Practive 5-2

--- src/5/TestButton.java
+++ src/5/TestButton.java
@@ -2,35 +2,29 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-class ButtonPanel extends JPanel {
+class ButtonPanel extends JPanel implements ActionListener {
   private JButton b1,b2,b3;
   private JLabel l;
-  private Button1Listener bl1;
-  private Button2Listener bl2;
-  private Button3Listener bl3;
 
   ButtonPanel(){
     b1 = new JButton("Button 1");
     b2 = new JButton("Button 2");
     b3 = new JButton("Button 3");
     l =  new JLabel("0",JLabel.CENTER);
-    bl1 = new Button1Listener();
-    bl2 = new Button2Listener();
-    bl3 = new Button3Listener();
-    b1.addActionListener(bl1);
-    b2.addActionListener(bl2);
-    b3.addActionListener(bl3);
+    b1.addActionListener(this);
+    b2.addActionListener(this);
+    b3.addActionListener(this);
+    b1.setActionCommand("1");
+    b2.setActionCommand("2");
+    b3.setActionCommand("3");
     setLayout(new GridLayout(2,2));
     add(b1); add(b2); add(b3); add(l);
   }
-  class Button1Listener implements ActionListener {
-    public void actionPerformed(ActionEvent e){ l.setText("1");}
-  }
-  class Button2Listener implements ActionListener {
-    public void actionPerformed(ActionEvent e){ l.setText("2");}
-  }
-  class Button3Listener implements ActionListener {
-    public void actionPerformed(ActionEvent e){ l.setText("3");}
+
+  @Override
+  public void actionPerformed(ActionEvent e){
+    String es=e.getActionCommand();
+    l.setText(es);
   }
 }
 
