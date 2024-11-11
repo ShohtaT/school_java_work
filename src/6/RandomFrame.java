4bd9394cf438f3946a9275882a39196c5c01edae
Takahashi Shota <shohh6119@gmial.com>
2024-11-11 17:28:43 +0900

    Done Practive 6-4

--- src/6/RandomFrame.java
+++ src/6/RandomFrame.java
@@ -4,25 +4,47 @@ import java.util.*;
 
 class Figure {
   protected int x,y,size;
+  protected Color color;
+  private final static Color cl[]={Color.RED, Color.BLUE, Color.GREEN, Color.BLACK};
   Figure() {
     x = (int)(Math.random()*450);
     y = (int)(Math.random()*450);
     size=(int)(Math.random()*30+20);
+    color=cl[(int)(Math.random() * cl.length)];
   }
   void draw(Graphics g) {}
 }
 
 class Circle extends Figure {
   void draw(Graphics g) {
-    g.setColor(Color.RED);
+    g.setColor(color);
     g.drawOval(x,y,size,size);
   }
 }
 
 class Box extends Figure {
   void draw(Graphics g) {
-    g.setColor(Color.BLUE);
+    g.setColor(color);
     g.drawRect(x,y,size,size);
+  }
+}
+
+class Cross extends Figure {
+  void draw(Graphics g) {
+    g.setColor(color);
+    // 線を二本引く
+    g.drawLine(x, y, x + size, y + size);
+    g.drawLine(x + size, y, x, y + size);
+  }
+}
+
+class Triangle extends Figure {
+  void draw(Graphics g) {
+    g.setColor(color);
+    // 3点を指定して三角形を描画
+    int[] xpoints = {x, x + size, x + size / 2};
+    int[] ypoints = {y + size, y + size, y};
+    g.drawPolygon(xpoints, ypoints, 3);
   }
 }
 
@@ -34,6 +56,8 @@ class RandomPanel extends JPanel {
     for(int i=0;i<NUM;i++){
       fig.add(new Box());
       fig.add(new Circle());
+      fig.add(new Cross());
+      fig.add(new Triangle());
     }
   }
   public void paintComponent(Graphics g) {
